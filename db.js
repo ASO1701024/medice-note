f1f0e9fe5582c116c0eaa38094594c76d86ed610
umezaki <1701001@st.asojuku.ac.jp>
2020-06-05 09:40:44 +0900

    fix db.js

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
-const mysql = require('mysql2');
-const connection = mysql.createConnection({
+const mysql = require('mysql2/promise');
+const connection = mysql.createPool({
     host: '',
     port: 3306,
     user: '',
