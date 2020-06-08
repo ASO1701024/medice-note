09432ca98afba248b8bb1b2f18dd61e623c90d6d
umezaki <1701001@st.asojuku.ac.jp>
2020-06-08 13:13:04 +0900

    delete unnecessary code

--- router/logout.js
+++ router/logout.js
@@ -1,7 +1,5 @@
 const Router = require('koa-router');
 const router = new Router();
-// const mysql = require('mysql');
-// const connection = require('../db');
 
 router.get('/logout', async (ctx, next) => {
     ctx.session = null;
