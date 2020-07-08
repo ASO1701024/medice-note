b77a96e6bd5420bef9463caaf62c99df9a3368a5
umezaki <1701001@st.asojuku.ac.jp>
2020-07-08 13:34:44 +0900

    fix notice list

--- public/js/notice-delete-alert.js
+++ public/js/notice-delete-alert.js
@@ -11,3 +11,31 @@ function noticeDelete(noticeId) {
         }
     });
 }
+
+function noticeToggleTrue(noticeId) {
+    swal({
+        title: '無効中',
+        text: 'この通知は無効状態です。有効化しますか？',
+        icon: 'warning',
+        buttons: ['キャンセル', '有効化'],
+        dangerMode: true
+    }).then(function (value) {
+        if (value) {
+            // location.href = '/medicine-delete/' + medicineId
+        }
+    });
+}
+
+function noticeToggleFalse(noticeId) {
+    swal({
+        title: '有効中',
+        text: 'この通知は有効状態です。無効化しますか？',
+        icon: 'warning',
+        buttons: ['キャンセル', '無効化'],
+        dangerMode: true
+    }).then(function (value) {
+        if (value) {
+            // location.href = '/medicine-delete/' + medicineId
+        }
+    });
+}
