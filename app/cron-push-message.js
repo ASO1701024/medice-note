316cfbe64e38f472d77dbd78ae2dd38b4a92bcea
ASO1701002 <1701002@st.asojuku.ac.jp>
2020-07-17 09:47:46 +0900

    fix bag

--- app/cron-push-message.js
+++ app/cron-push-message.js
@@ -1,6 +1,6 @@
 const cron = require('node-cron');
 const connection = require('./db');
-const config = require('../config.json')
+const config = require('../config.json');
 const line = require('@line/bot-sdk');
 const client = new line.Client(config.line_bot);
 
@@ -8,22 +8,22 @@ module.exports = cron.schedule('0,30 * * * *', async () => {
     let dateObject = new Date();
     let date = formatDate(dateObject, 'yyyy-MM-dd');
     let time = formatDate(dateObject, 'HH:mm:00');
-    let dayOfWeek = dateObject.getDay()
+    let dayOfWeek = dateObject.getDay();
 
     let getNoticeListSQL =
         'SELECT SQ3.notice_id ' +
         'FROM (SELECT notice_id FROM notice_time WHERE notice_time = ?)SQ ' +
-            'RIGHT JOIN (SELECT notice_id FROM notice_day WHERE day_of_week = ?)SQ2 ON SQ.notice_id = SQ2.notice_id ' +
-            'LEFT JOIN (SELECT notice_id, notice_period FROM notice WHERE is_enable = true)SQ3 ON SQ.notice_id = SQ3.notice_id ' +
+        'RIGHT JOIN (SELECT notice_id FROM notice_day WHERE day_of_week = ?)SQ2 ON SQ.notice_id = SQ2.notice_id ' +
+        'LEFT JOIN (SELECT notice_id, notice_period FROM notice WHERE is_enable = true)SQ3 ON SQ.notice_id = SQ3.notice_id ' +
         'WHERE SQ3.notice_period >= ?;';
     // let noticeList = (await connection.query(getNoticeListSQL, ['00:00:00',dayOfWeek,date]))[0];
-    let noticeList = (await connection.query(getNoticeListSQL, [time,dayOfWeek,date]))[0];
+    let noticeList = (await connection.query(getNoticeListSQL, [time, dayOfWeek, date]))[0];
 
     await pushNotice(noticeList);
 });
 
 // format 'Date' object's output
-function formatDate (date, format) {
+function formatDate(date, format) {
     format = format.replace(/yyyy/g, date.getFullYear());
     format = format.replace(/MM/g, ('0' + (date.getMonth() + 1)).slice(-2));
     format = format.replace(/dd/g, ('0' + date.getDate()).slice(-2));
@@ -32,27 +32,27 @@ function formatDate (date, format) {
     format = format.replace(/ss/g, ('0' + date.getSeconds()).slice(-2));
     format = format.replace(/SSS/g, ('00' + date.getMilliseconds()).slice(-3));
     return format;
-};
+}
 
 // push notice message to line_user in noticeList
-async function pushNotice (noticeList) {
+async function pushNotice(noticeList) {
     for (let noticeRow of noticeList) {
         let to = await getLineUserId(noticeRow['notice_id']);
         let message = await createPushMessage(noticeRow['notice_id']);
         await client.pushMessage(to, message, false)
-            .then(async ()=>{
+            .then(async () => {
                 let message = '機能: pushNotice  ステータスコード: 200';
                 await insertUserMessage(noticeRow['notice_id'], message, 2);
             })
             .catch(async (error) => {
-                let errorMsg = '機能: pushNotice  ステータスコード: ' + error['statusCode'] +'  エラーメッセージ: '+ error['statusMessage'];
+                let errorMsg = '機能: pushNotice  ステータスコード: ' + error['statusCode'] + '  エラーメッセージ: ' + error['statusMessage'];
                 await insertUserMessage(noticeRow['notice_id'], errorMsg, 3);
             })
     }
 }
 
 // get line_user_id to push message
-async function getLineUserId(noticeId){
+async function getLineUserId(noticeId) {
     let getLineUserIdSQL =
         'SELECT line_user_id ' +
         'FROM (SELECT user_id FROM notice N WHERE notice_id = ?)SQ ' +
@@ -61,7 +61,7 @@ async function getLineUserId(noticeId){
 }
 
 // create the text to push message
-async function createPushMessage(noticeId){
+async function createPushMessage(noticeId) {
     let getNoticeDataSQL =
         'SELECT SQ.notice_name, M.medicine_name, M.number ' +
         'FROM (' +
@@ -70,8 +70,8 @@ async function createPushMessage(noticeId){
         'LEFT JOIN medicine M on NM.medicine_id = M.medicine_id;';
     let noticeData = (await connection.query(getNoticeDataSQL, [noticeId]))[0];
 
-    let messageText = '['+ noticeData[0]['notice_name'] +']\n';
-    for (let row of noticeData){
+    let messageText = '[' + noticeData[0]['notice_name'] + ']\n';
+    for (let row of noticeData) {
         messageText += '・' + row['medicine_name'];
         messageText += ': ';
         messageText += row['number'];
@@ -86,7 +86,7 @@ async function createPushMessage(noticeId){
 
 async function insertUserMessage(noticeId, resultMessage, resultFlg) {
     let getUserIdSQL = 'SELECT user_id FROM notice WHERE notice_id = ?;';
-    let userId = (await connection.query(getUserIdSQL,[noticeId]));
+    let userId = (await connection.query(getUserIdSQL, [noticeId]))[0][0]['user_id'];
 
     let insertErrorSQL = 'INSERT INTO user_message VALUES (0,?,?,?);';
     await connection.query(insertErrorSQL, [userId, resultMessage, resultFlg]);
