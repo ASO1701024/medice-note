6a0d653f50e3b5db966b4aaa293b4217bda9b28b
umezaki <1701001@st.asojuku.ac.jp>
2020-10-07 13:59:01 +0900

    fix popup window left

--- public/js/notice-register.js
+++ public/js/notice-register.js
@@ -52,6 +52,6 @@ $(document).on('click', '.action-week-all-deselect', function () {
 $(document).on('click', '.medicine-popup-button', function () {
     let button = $(this)[0];
     let medicineId = $(button).data('medicine-id');
-    let params = 'scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,width=600,height=700,left=100,top=100';
+    let params = 'scrollbars=no,resizable=yes,status=no,location=no,toolbar=no,menubar=no,width=600,height=700,left=200,top=100';
     open(`/medicine/${medicineId}`, 'お薬情報', params);
 })
