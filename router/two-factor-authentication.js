2748bcc8757f037d6ca707618f46b3c3cb726658
umezaki <1701001@st.asojuku.ac.jp>
2020-10-12 14:27:09 +0900

    add routing

---
+++ router/two-factor-authentication.js
@@ -0,0 +1,9 @@
+const Router = require('koa-router');
+const router = new Router();
+const connection = require('../app/db');
+
+router.get('/two-factor-authentication', async (ctx) => {
+    
+});
+
+module.exports = router;
